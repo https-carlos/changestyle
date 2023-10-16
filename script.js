91d693b62aefdf085ddaadeee5d820156057ba2a
https-carlos <139911150+https-carlos@users.noreply.github.com>
2023-10-16 10:44:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function js_style() {
+    let text = document.getElementById("text")
+    let imagem = document.getElementById("imagem")
+    text.style.color = "purple" 
+    text.style.fontFamily = "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif";
+    text.style.fontSize = "30px"
+
+}
